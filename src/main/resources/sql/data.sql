8783e88a20e2e38cb7ec9482ca95510d0f3b093c
이종학 <jhlee@jhlee.local>
2019-06-03 10:53:25 +0900

    초기 데이터 생성 script 추가

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO member(email, password, name) VALUES ('gildong.hong@example.com', '1234', '홍길동');
+INSERT INTO member(email, password, name) VALUES ('ginsan.jang@example.com', '1234', '장길산');
+INSERT INTO member(email, password, name) VALUES ('kuchjung.lim@example.com', '1234', '임꺽정');
