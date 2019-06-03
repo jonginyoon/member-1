a17b05d32dade97e8d4e6ba7ade09794b2e3dc6e
이종학 <jhlee@jhlee.local>
2019-06-03 13:51:44 +0900

    Member repository에 이름을 이용한 쿼리 method 생성

--- src/main/java/com/example/member/repository/MemberRepository.java
+++ src/main/java/com/example/member/repository/MemberRepository.java
@@ -3,5 +3,9 @@ package com.example.member.repository;
 import com.example.member.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findAllByNameContains(String name);
 }
